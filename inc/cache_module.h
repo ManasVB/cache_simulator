89546e0a809c3d2739e8bb043ce8e15708a85fd4
Manas Buzruk <66116717+ManasVB@users.noreply.github.com>
2024-09-07 21:49:14 -0400

    Read implementation (#3)
    * save
    
    * sasve

--- inc/cache_module.h
+++ inc/cache_module.h
@@ -19,20 +19,20 @@ class CacheModule {
   uint8_t tagoffset;
   uint8_t tagbits;
 
-  MetaData **metadata;
-
-  typedef enum {
-    HIT = true,
-    MISS = false
-  } CacheStatus;
-
   public:
+
+  MetaData **metadata;
+  uint32_t **tagArray;
+
+  uint32_t Cache_Hits, Cache_Misses;
 
   CacheModule *next_node;  // linked-list impl to link to next level of hierarchy
 
   CacheModule(uint32_t, uint32_t, uint32_t);
   ~CacheModule() = default;
 
-  uint32_t getTagAddr(uint32_t);
+  uint32_t parseAddress(uint32_t, uint16_t &);
+
+  uint32_t setAssoc(void);
 
 };
--- src/cache_module.cpp
+++ src/cache_module.cpp
@@ -6,22 +6,32 @@
 extern CacheModule *head_node;
 
 CacheModule::CacheModule(uint32_t blocksize, uint32_t cache_size, uint32_t assoc) {
+  
+  // Set the cache parameters
   this->blocksize = blocksize;
   this->cache_size = cache_size;
   this->assoc = assoc;
   sets = (cache_size)/(blocksize * assoc);
+  std::cout << "Sets = " << sets << std::endl;
 
   blockoffsetbits = log2(blocksize);
   indexbits = log2(sets);
   tagoffset = blockoffsetbits + indexbits;
   tagbits = 32 - tagoffset;
 
-  // Initialize the metadata
+  // Initialize the metadata and tagArray
   metadata = new MetaData*[sets];
+  tagArray = new uint32_t*[sets];
   for(uint32_t i=0; i < sets; ++i) {
     metadata[i] = new MetaData[assoc];
-    (void)memset(metadata[i], 0, (assoc) * sizeof(MetaData));
+    (void)memset(metadata[i], 0, assoc * sizeof(MetaData));
+
+    tagArray[i] = new uint32_t[assoc];
+    (void)memset(tagArray[i], 0, assoc * sizeof(uint32_t));
   }
+
+  // Initialize hits and misses to zero
+  Cache_Hits = Cache_Misses = 0;
 
   // Cache linked list implementation
   if(head_node == nullptr) {
@@ -36,6 +46,13 @@ CacheModule::CacheModule(uint32_t blocksize, uint32_t cache_size, uint32_t assoc
   }
 }
 
-uint32_t CacheModule::getTagAddr(uint32_t Addr) {
+uint32_t CacheModule::parseAddress(uint32_t Addr, uint16_t &index) {
+  
+  uint32_t mask = (1 << tagoffset) - 1;
+  index = (Addr & mask) >> blockoffsetbits;
   return (Addr >> (tagoffset));
+}
+
+uint32_t CacheModule::setAssoc(void) {
+  return this->assoc;
 }
